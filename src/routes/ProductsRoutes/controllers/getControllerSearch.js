e5863212165d4abedeacb03c924501e148c6d973
anfevera <116735342+anfevera97@users.noreply.github.com>
2023-06-16 17:27:02 -0500

    size filter fix

--- src/routes/ProductsRoutes/controllers/getControllerSearch.js
+++ src/routes/ProductsRoutes/controllers/getControllerSearch.js
@@ -5,8 +5,16 @@ const getControllerSearch = async (page, query) => {
 	const filter = {};
 	for (const key in query) {
 		if (query.hasOwnProperty(key)) {
-			if (['color', 'size', 'brand', 'season'].includes(key)) {
-				filter[key] = { $in: query[key].split(',') };
+			if (['color', 'brand', 'season'].includes(key)) {
+				filter[key] = {
+					$in: query[key].split(','),
+				};
+			} else if (key === 'size') {
+				const sizes = query[key];
+				const regexSizes = sizes.split(',').join('|');
+				filter[key] = {
+					$regex: new RegExp(`\\b(${regexSizes})\\b`, 'i'),
+				};
 			} else if (['category', 'gender', 'name'].includes(key)) {
 				filter[key] = { $regex: new RegExp(query[key], 'i') };
 			}
