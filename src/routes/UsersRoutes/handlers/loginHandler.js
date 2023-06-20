e699090040452104916cd489708e3ba62d2e819d
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-20 09:58:10 -0300

    login

--- src/routes/UsersRoutes/handlers/loginHandler.js
+++ src/routes/UsersRoutes/handlers/loginHandler.js
@@ -3,7 +3,7 @@ const loginController = require('../controllers/loginControlleer')
 const loginHandler = async(req, res) =>{
     try{
         const {password, email} = req.query
-        let info = await loginController(password, emial)
+        let info = await loginController(password, email)
         res.status(200).json(info)
     }catch(err){
         res.status(400).json({error:err.message})
