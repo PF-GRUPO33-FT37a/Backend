4acd73e2da25e9c6c9d44a6797b03d6cb7def9a0
nrcs97 <franciscoserracorrea@gmail.com>
2023-06-20 04:10:28 -0300

    fix Products routes issue

--- src/routes/ProductsRoutes/controllers/getControllerSearch.js
+++ src/routes/ProductsRoutes/controllers/getControllerSearch.js
@@ -3,38 +3,56 @@ const modelateDataPaginado = require('../../../utils/modelateDataPaginate');
 
 const getControllerSearch = async (page, query) => {
 
-const filter = {};
-	for (const key in query) {
-		if (query.hasOwnProperty(key)) {
-			if (['color', 'brand', 'season'].includes(key)) {
-				filter[key] = {
-					$in: query[key].split(','),
-				};
-			} else if (key === 'size') {
-				const sizes = query[key];
-				const regexSizes = sizes.split(',').join('|');
-				filter[key] = {
-					$regex: new RegExp(`\\b(${regexSizes})\\b`, 'i'),
-				};
-			} else if (['category', 'gender', 'name'].includes(key)) {
-				filter[key] = { $regex: new RegExp(query[key], 'i') };
-			}
-		}
-	}
+	// const filter = {};
+	// 	for (const key in query) {
+	// 		if (query.hasOwnProperty(key)) {
+	// 			if (['color', 'brand', 'season'].includes(key)) {
+	// 				filter[key] = {
+	// 					$in: query[key].split(','),
+	// 				};
+	// 			} else if (key === 'size') {
+	// 				const sizes = query[key];
+	// 				const regexSizes = sizes.split(',').join('|');
+	// 				filter[key] = {
+	// 					$regex: new RegExp(`\\b(${regexSizes})\\b`, 'i'),
+	// 				};
+	// 			} else if (['category', 'gender', 'name'].includes(key)) {
+	// 				filter[key] = { $regex: new RegExp(query[key], 'i') };
+	// 			}
+	// 		}
+	// 	}
 
-	const queryResult = await Products.find(filter);
-  const products = [];
-  for (const product of queryResult) {
-    let code = product.articleCode
-    const sameCodeProducts = await Products.find({
-      $and: [{ _id: { $ne: product.id } }, { articleCode: code }],
-    });
-    const productWithSameCode = { ...product.toObject(), sameCode: sameCodeProducts };
-    delete productWithSameCode._doc;
-    products.push(productWithSameCode);
-  }
-  const response = modelateDataPaginado(page, products);
-  return response;
+	// 	const queryResult = await Products.find(filter);
+	//   const products = [];
+	//   for (const product of queryResult) {
+	//     let code = product.articleCode
+	//     const sameCodeProducts = await Products.find({
+	//       $and: [{ _id: { $ne: product.id } }, { articleCode: code }],
+	//     });
+	//     const productWithSameCode = { ...product.toObject(), sameCode: sameCodeProducts };
+	//     delete productWithSameCode._doc;
+	//     products.push(productWithSameCode);
+	//   }
+	//   const response = modelateDataPaginado(page, products);
+	// -------
+	const resultProducts = await Products.find(query)
+	let response = []
+
+	for (const i in resultProducts) {
+		const sameCode = await Products.find({ ...query,
+			articleCode: resultProducts[i].articleCode,
+			_id: {$ne: resultProducts[i]._id}})
+			
+		const { _id, name, category, gender, size, color, season,
+			images, stock, isActive, brand, price, articleCode, __v } = resultProducts[i]
+
+		response.push({
+			_id, name, category, gender, size, color, season, images,
+			stock, isActive, brand, price, articleCode, sameCode: sameCode, __v
+		})
+	}
+	
+  	return response;
 };
 
 
