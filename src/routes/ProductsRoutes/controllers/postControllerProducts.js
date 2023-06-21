68026230684d820508cd0ce494d203010379741e
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-21 18:17:34 -0300

    cambio size

--- src/routes/ProductsRoutes/controllers/postControllerProducts.js
+++ src/routes/ProductsRoutes/controllers/postControllerProducts.js
@@ -1,13 +1,22 @@
 const Products = require('../../../db/models/productSchema');
 
 const postControllerProduct = async (data, firebaseUrls) => {
-	const stockSum = data.size.reduce((acc, obj) => acc + obj.stock, 0);
-
+	console.log(data)
+	let stockSum = 0;
+	console.log(data.size)
+	let arr = []
+    for (let i = 0; i < data.size.length; i++) {
+		const obj = JSON.parse(data.size[i])
+		arr.push(obj)
+		console.log(obj)
+        stockSum += obj.stock;
+    }
+	console.log(stockSum)
 	const product = {
 		name: data.name,
 		category: data.category,
 		gender: data.gender,
-		size: data.size,
+		size: arr,
 		color: data.color,
 		season: data.season,
 		images: firebaseUrls,
