b625f234e5cf91bb3f7e405d4ac4bfc872ee8a96
nrcs97 <franciscoserracorrea@gmail.com>
2023-06-22 13:01:20 -0300

    getHandlerReviews.js catch block

--- src/routes/ReviewsRoutes/handlers/getHandlerReviews.js
+++ src/routes/ReviewsRoutes/handlers/getHandlerReviews.js
@@ -5,7 +5,7 @@ const getHandlerReviews = async (req, res)=> {
         const response = await getControllerReviews()
         res.status(200).json(response)
     } catch (err){
-
+        res.status(400).json({ error: err.message})
     }
 }
 
